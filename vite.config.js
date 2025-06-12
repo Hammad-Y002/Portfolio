8076b3c40d0a098e4d1057bbcf70abc8e1dd4413
HammadYaseen666 <f21binft1m01073@iub.edu.pk>
2025-06-12 15:48:47 +0500

    revert changes of index and vite config files

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-  },
+
 });
