5d5640dba3740219c244b639600040de1f4cd7d5
HammadYaseen666 <f21binft1m01073@iub.edu.pk>
2025-06-12 16:36:00 +0500

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,9 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-
+  }
 });
