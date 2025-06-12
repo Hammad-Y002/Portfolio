f5a09de8351156ef3d620cf490dca4045b9d57cd
HammadYaseen666 <f21binft1m01073@iub.edu.pk>
2025-06-12 15:36:58 +0500

    Change the base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio/",
+  base: "/Portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
